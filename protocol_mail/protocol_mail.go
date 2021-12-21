cd0af87072dcb0281e2b05ee67bb5aaf1e0dac7f
胡佳 <hujia@hujiadeMacBook-Pro.local>
2021-12-21 17:45:27 +0800

    update

--- protocol_mail/protocol_mail.go
+++ protocol_mail/protocol_mail.go
@@ -2,15 +2,12 @@ package protocol_mail
 
 import (
 	"fmt"
-	//"strings"
 
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
-	//"net/mail"
-	//"github.com/jhillyerd/go.enmime"
 	"github.com/elastic/beats/v7/libbeat/processors"
 	jsprocessor "github.com/elastic/beats/v7/libbeat/processors/script/javascript/module/processor"
 )
@@ -46,41 +43,10 @@ func New(cfg *common.Config) (processors.Processor, error) {
 }
 
 func (p *ProtocolMail) Run(event *beat.Event) (*beat.Event, error) {
-	sf, err := event.GetValue(p.SourceField)
-	if err != nil {
-		if p.IgnoreFailure || (p.IgnoreMissing && errors.Cause(err) == common.ErrKeyNotFound) {
-			return event, nil
-		}
-		return event, errors.Wrapf(err, "failed to get source field %s", p.SourceField)
-	}
+	p.log.Debugf("========== ProtocolMail Start ==========")
 
-	sfStr, ok := sf.(string)
-	if !ok {
-		if p.IgnoreFailure {
-			return event, nil
-		}
-		return event, errors.New("failed to parse source field")
-	}
 
-	p.log.Debugf("split source field: %s", sfStr)
-	_, err = event.PutValue(p.TargetField, "test")
-	if err != nil {
-		if p.IgnoreFailure {
-			return event, nil
-		}
-		return event, errors.Wrapf(err, "failed to put event value key: %s, value: %s", p.TargetField, sfStr)
-	}
-
-	if p.ProcessorsField != "" {
-		_, err = event.PutValue(p.ProcessorsField, true)
-		if err != nil {
-			if p.IgnoreFailure {
-				return event, nil
-			}
-			return event, errors.Wrapf(err, "failed to put event value key: %s, value: %t", p.ProcessorsField, true)
-		}
-	}
-
+	p.log.Debugf("========== ProtocolMail End ==========")
 	return event, nil
 }
 
